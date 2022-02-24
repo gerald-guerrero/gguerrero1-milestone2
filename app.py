4d270871a4990915ece228ee42e804edaa735e1e
Gerald Guerrero <sbgeraldg@gmail.com>
2022-02-23 23:34:09 -0500

    Added functional comment/rating form. Username is recorded as  Guest until accounts are implemented

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
 import os
+
+from sqlalchemy import true
 from TMDB import movie_data
 from WIKI import wiki_data
 import random
@@ -18,11 +20,11 @@ db = SQLAlchemy(app)
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
 
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
+    username = db.Column(db.String(100))
     movie_id = db.Column(db.Integer)
     rating = db.Column(db.Integer)
     comment = db.Column(db.String(750))
@@ -35,17 +37,38 @@ def index():
     id_list.append(604)
     id_list.append(155)
 
-    title, tagline, genres, image = movie_data(random.choice(id_list))
+    movie_id = random.choice(id_list)
+    title, tagline, genres, image = movie_data(movie_id)
     url = wiki_data(title)
+
+    comments = []
+    comments = Comments.query.filter_by(movie_id=movie_id).all()
+ 
     
     return flask.render_template(
         "index.html",
+        movie_id=movie_id,
         title=title,
         tagline=tagline,
         genres=genres,
         image=image,
-        url=url
+        url=url,
+        comments=comments
     )
+@app.route('/save', methods=["GET", "POST"])
+def save():
+    if flask.request.method == "POST":
+        input = flask.request.form
+        username = "Guest"
+        movie_id = input.get("movie id")
+        print(movie_id)
+        rating = int(input.get("rating"))
+        comment = input.get("comment")
+
+        new_comment = Comments(username=username, movie_id=movie_id, rating=rating, comment=comment)
+        db.session.add(new_comment)
+        db.session.commit()
+    return flask.redirect(flask.url_for("index"))
 
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
